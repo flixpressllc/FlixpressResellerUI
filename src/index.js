7525f94c4953d379e31966c45dd74d7f598dcb52
Don Denton <don@happycollision.com>
2017-02-28 13:00:17 -0600

    setup window env (fetch and Promise)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+// window env stuff first
+import './utils/env/promise';
+import './utils/env/fetch';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from './components/App';
 
 ReactDOM.render(
   <App />,
